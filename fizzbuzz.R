e252252fe7e9e02650d4e010c0057e6d351b15a6
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-02-12 09:30:54 -0500

    Update fizzbuzz.R

--- fizzbuzz.R
+++ fizzbuzz.R
@@ -1,3 +1,18 @@
 # Fizzbuzz in R 
 
-for (i in 1
+for (i in 1:100){
+  if(i%%3 == 0 & 1%%5 == 0) {
+    print('FizzBuzz')
+ }
+  else if(i%%3 == 0) {
+    print('Fizz')
+ }
+    else if (i%%5 == 0){
+      print('Buzz')
+ }
+  else {
+    print(i)
+ }
+
+}
+  
